bf582c0eb8cee444210aa603e8c21417fb471ed1
Inno <shehzadmalik123.sm@gmail.com>
2024-10-08 17:41:29 +0530

    feat: added store and blogslice

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import { Provider } from 'react-redux'
 import './App.css'
 import { AllRoutes } from './routes/Allroutes'
+import { store } from './redux/store'
 
 function App() {
   return (
+ 
     <AllRoutes/>
+  
   )
 }
 
