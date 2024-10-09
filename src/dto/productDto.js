b6a28c8d0e79df25c6bb311ff2f8bc10c1537b94
blackcode1996 <aakashmukherjee1996@gmail.com>
2024-10-09 14:34:15 +0530

    feat(homePage): whole_home_page_done

---
+++ src/dto/productDto.js
@@ -0,0 +1,17 @@
+import { trimText } from "../utils/common";
+
+const productDto = (data, limit) => {
+  const limitedData = limit && data.length ? data.slice(0, limit) : data;
+
+  return limitedData.map((item) => ({
+    productId: item.id,
+    productImage: item.image,
+    productPrice: parseFloat(item.price).toFixed(1), 
+    productDiscountPercent: 10,
+    productTitle: trimText(item.title),
+    productRating: item.rating.rate,
+    productActualPrice: parseFloat(item.price + (0.1 * item.price)).toFixed(1), 
+  }));
+};
+
+export default productDto;
