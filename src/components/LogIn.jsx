f32b0360a6a77a885e0736889294c994e516cfea
BhardwajVarsha <141000865+BhardwajVarsha@users.noreply.github.com>
2024-10-10 22:56:00 +0530

    Changes Added In Auth

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -1,9 +1,128 @@
-import React from 'react'
+/* eslint-disable react/prop-types */
+import { Formik } from "formik";
+import { validateEmail, validatePassword } from "../utils/constants";
+import { useNavigate } from "react-router-dom";
+import { loginUser } from "../utils/firebase";
+import { toast } from "react-toastify";
 
-const LogIn = () => {
+const LoginForm = () => {
+  const navigate = useNavigate();
+
   return (
-    <div>LogIn</div>
-  )
-}
+    <Formik
+      initialValues={{ email: "", password: "" }}
+      validate={(values) => {
+        const errors = {};
 
-export default LogIn
+        const emailValidation = validateEmail(values.email);
+        if (emailValidation.error) {
+          errors.email = emailValidation.message;
+        }
+
+        const passwordValidation = validatePassword(values.password);
+        if (passwordValidation.error) {
+          errors.password = passwordValidation.message;
+        }
+        return errors;
+      }}
+      onSubmit={async (values) => {
+        try {
+          const { success, message } = await loginUser(
+            values.email,
+            values.password
+          ); // Await the loginUser function
+
+          if (success) {
+            toast.success(message);
+            navigate("/profile");
+          } else {
+            toast.error(message);
+          }
+        } catch (error) {
+          toast.error("An error occurred during login.", error);
+        }
+      }}
+    >
+      {({
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        isSubmitting,
+        resetForm,
+      }) => (
+        <form
+          onSubmit={handleSubmit}
+          className="bg-white p-6 sm:p-8 rounded-lg shadow-md max-w-sm sm:max-w-md md:max-w-lg lg:max-w-l mx-auto my-8 sm:my-16"
+        >
+          <h2 className="text-xl flex justify-center text-primary w-full py-1 sm:text-2xl font-bold mb-4 sm:mb-6 text-center">
+            Log In
+          </h2>
+          <div className="mb-4">
+            <label className="block text-gray-700 font-medium mb-2">
+              Email
+            </label>
+            <input
+              type="email"
+              name="email"
+              value={values.email}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              className={`w-full p-2 sm:p-2 border ${
+                touched.email && errors.email
+                  ? "border-red-500"
+                  : "border-gray-300"
+              } rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500`}
+            />
+            {touched.email && errors.email ? (
+              <div className="text-red-500 text-sm mt-1">{errors.email}</div>
+            ) : null}
+          </div>
+          <div className="mb-6">
+            <label className="block text-gray-700 font-medium mb-2">
+              Password
+            </label>
+            <input
+              type="password"
+              name="password"
+              value={values.password}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              className={`w-full p-2 sm:p-2 border ${
+                touched.password && errors.password
+                  ? "border-red-500"
+                  : "border-gray-300"
+              } rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500`}
+            />
+            {touched.password && errors.password ? (
+              <div className="text-red-500 text-sm mt-1">{errors.password}</div>
+            ) : null}
+          </div>
+          <button
+            onClick={handleSubmit}
+            type="submit"
+            className="w-full bg-primary text-neutral font-bold py-2 px-4 rounded-md hover:bg-blue-600 transition duration-200"
+          >
+            {isSubmitting ? "Submitting" : "Submit"}
+          </button>
+          <div className="my-2 flex w-full justify-center">
+            Create New Account
+            <span
+              className="underline cursor-pointer mx-3"
+              onClick={() => {
+                navigate("/signup");
+                resetForm();
+              }}
+            >
+              SignUp
+            </span>
+          </div>
+        </form>
+      )}
+    </Formik>
+  );
+};
+
+export default LoginForm;
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -18,83 +18,77 @@ const Profile = () => {
       }
       else {
         localStorage.removeItem('token')
-        navigate('/auth')
-
+        navigate('/login')
       }
     })
   }, [])
 
-
-
- 
-
-
   return (
     <div>
 
-      <div class="bg-white w-full flex flex-col gap-5 px-3 md:px-16 lg:px-28 md:flex-row text-[#161931]">
-        <aside class="hidden py-4 md:w-1/3 lg:w-1/4 md:block">
-          <div class="sticky flex flex-col gap-2 p-4 text-sm  top-12">
+      <div className="bg-white w-full flex flex-col gap-5 px-3 md:px-16 lg:px-28 md:flex-row text-[#161931]">
+        <aside className="hidden py-4 md:w-1/3 lg:w-1/4 md:block">
+          <div className="sticky flex flex-col gap-2 p-4 text-sm  top-12">
 
-            <h2 class="pl-3 mb-4 text-2xl font-semibold">Settings</h2>
+            <h2 className="pl-3 mb-4 text-2xl font-semibold">Settings</h2>
 
-            <a href="#" class="flex items-center justify-center px-3 py-2.5 font-bold bg-white  text-indigo-900 border rounded-full">
+            <a href="#" className="flex items-center justify-center px-3 py-2.5 font-bold bg-white  text-indigo-900 border rounded-full">
               Profile
             </a>
           </div>
         </aside>
-        <main class="w-full min-h-screen py-1 md:w-2/3 lg:w-3/4">
-          <div class="p-2 md:p-4 border-l-2">
-            <div class="w-full px-6 pb-8 mt-8 sm:max-w-xl sm:rounded-lg">
-              <h2 class="pl-6 text-2xl font-bold sm:text-xl">Public Profile</h2>
+        <main className="w-full min-h-screen py-1 md:w-2/3 lg:w-3/4">
+          <div className="p-2 md:p-4 border-l-2">
+            <div className="w-full px-6 pb-8 mt-8 sm:max-w-xl sm:rounded-lg">
+              <h2 className="pl-6 text-2xl font-bold sm:text-xl">Public Profile</h2>
 
-              <div class="grid max-w-xl mx-auto mt-8">
-                <div class="flex justify-between flex-col items-center space-y-5 sm:flex-row sm:space-y-0">
-                  <div class="flex items-center justify-center object-cover w-40 h-40 p-2 rounded-full ring-2 ring-indigo-300 bg-primary text-neutral  dark:ring-indigo-500 bg-indigo-200">
-                    <span class="text-7xl font-bold text-indigo-700">
+              <div className="grid max-w-xl mx-auto mt-8">
+                <div className="flex justify-between flex-col items-center space-y-5 sm:flex-row sm:space-y-0">
+                  <div className="flex items-center justify-center object-cover w-40 h-40 p-2 rounded-full ring-2 ring-indigo-300 bg-primary text-neutral  dark:ring-indigo-500 bg-indigo-200">
+                    <span className="text-7xl font-bold text-indigo-700">
                       {userData?.displayName?.charAt(0)}
                       </span>
                   </div>
 
-                  <div class="flex flex-col space-y-5 sm:ml-8">
+                  <div className="flex flex-col space-y-5 sm:ml-8">
                     <button type="button" onClick={()=>navigate('/profile/updatePassword')}
-                      class="py-3.5 px-7 text-base font-medium text-primary focus:outline-none bg-white rounded-lg border border-indigo-200 hover:bg-indigo-100 hover:text-[#202142] focus:z-10 focus:ring-4 focus:ring-indigo-200 ">
+                      className="py-3.5 px-7 text-base font-medium text-primary focus:outline-none bg-white rounded-lg border border-indigo-200 hover:bg-indigo-100 hover:text-[#202142] focus:z-10 focus:ring-4 focus:ring-indigo-200 ">
                       Update Password
                     </button>
                     <button type="button" onClick={() => signOut(auth)}
-                      class="py-3.5 px-7 text-base font-medium text-neutral focus:outline-none bg-primary rounded-lg border border-indigo-200 hover:bg-indigo-900 focus:z-10 focus:ring-4 focus:ring-indigo-200  ">
+                      className="py-3.5 px-7 text-base font-medium text-neutral focus:outline-none bg-primary rounded-lg border border-indigo-200 hover:bg-indigo-900 focus:z-10 focus:ring-4 focus:ring-indigo-200  ">
                       Log Out
                     </button>
                   </div>
                 </div>
 
-                <div class="items-center mt-8 sm:mt-14 text-[#202142]">
+                <div className="items-center mt-8 sm:mt-14 text-[#202142]">
 
                   <div
-                    class="flex flex-col items-center w-full mb-2 space-x-0 space-y-2 sm:flex-row sm:space-x-4 sm:space-y-0 sm:mb-6">
-                    <div class="w-full">
-                      <label for="first_name"
-                        class="block mb-2 text-l text-primary font-bold ">User Name</label>
+                    className="flex flex-col items-center w-full mb-2 space-x-0 space-y-2 sm:flex-row sm:space-x-4 sm:space-y-0 sm:mb-6">
+                    <div className="w-full">
+                      <label htmlFor="first_name"
+                        className="block mb-2 text-l text-primary font-bold ">User Name</label>
                       <input type="text" id="first_name"
-                        class="bg-indigo-50 border border-indigo-300 text-indigo-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2.5 "
+                        className="bg-indigo-50 border border-indigo-300 text-indigo-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2.5 "
                         placeholder="Your full name" value={userData?.displayName}
                         disabled
                         required />
                     </div>
                   </div>
-                  <div class="mb-2 sm:mb-6">
-                    <label for="email"
-                      class="block mb-2 text-l font-semibold text-primary dark:text-primary">Registered
+                  <div className="mb-2 sm:mb-6">
+                    <label htmlFor="email"
+                      className="block mb-2 text-l font-semibold text-primary dark:text-primary">Registered
                       email</label>
                     <input type="email" id="email"
                       value={userData.email}
                       disabled
-                      class="bg-indigo-50 border border-indigo-300 text-indigo-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2.5 "
+                      className="bg-indigo-50 border border-indigo-300 text-indigo-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2.5 "
                       placeholder="your.email@mail.com" required />
                   </div>
-                  <div class="flex justify-end">
+                  <div className="flex justify-end">
                     <button type="submit"
-                      class="text-white bg-indigo-700  hover:bg-indigo-800 focus:ring-4 focus:outline-none focus:ring-indigo-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-indigo-800">Save</button>
+                      className="text-white bg-indigo-700  hover:bg-indigo-800 focus:ring-4 focus:outline-none focus:ring-indigo-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-indigo-800">Save</button>
                   </div>
 
                 </div>
